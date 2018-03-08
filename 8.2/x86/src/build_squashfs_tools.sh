38e855c5b2d9c9d488c43fa696f94a5c6dc822a9
Michael B. Sumulong <msumulong@gmail.com>
2018-03-08 17:23:36 -0500

    Pass TinyCore version to build scripts and docker builds
    This will allow for removing redundant code eventually.

--- 8.2/x86/src/build_squashfs_tools.sh
+++ 8.2/x86/src/build_squashfs_tools.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# $1 parameter is the TinyCore Linux major version
+# $2 parameter is the TinyCore Linux architecture
+set -e
 
 apk add --no-cache curl squashfs-tools >/dev/null
 
 if [ ! -f squashfs-tools.tcz ]; then
-    curl -SLO http://tinycorelinux.net/8.x/x86/tcz/squashfs-tools.tcz >/dev/null
+    curl -SLO http://tinycorelinux.net/$1.x/$2/tcz/squashfs-tools.tcz >/dev/null
 fi
 
 [ -d /mnt ] || mkdir /mnt
